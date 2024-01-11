6118ed6f985993b2095cbf067b2e04890f065936
Ruykaji <ilya.tanc@gmail.com>
2024-01-11 21:48:59 +0300

    Move to cmake and gtest

---
+++ build/data_structures/doubly_linked_list/CMakeFiles/doubly_linked_list.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/doubly_linked_list.dir/test.cpp.o"
+  "CMakeFiles/doubly_linked_list.dir/test.cpp.o.d"
+  "doubly_linked_list"
+  "doubly_linked_list.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/doubly_linked_list.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
