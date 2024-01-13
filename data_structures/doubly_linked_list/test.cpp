ae1f91dcf2753005d6a39676823269da6e724a49
Ruykaji <ilya.tanc@gmail.com>
2024-01-13 22:32:14 +0300

    Made doubly-linked-list in cpp + update cmake for tests

--- data_structures/doubly_linked_list/test.cpp
+++ data_structures/doubly_linked_list/test.cpp
@@ -1,1 +1,14 @@
 #include <gtest/gtest.h>
+
+#include "doubly_linked_list.hpp"
+
+TEST(CPP_Doubly_Linked_List, Create_Empty_List)
+{
+    DoublyLinkedList<int32_t> list {};
+}
+
+int main(int argc, char** argv)
+{
+    testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
