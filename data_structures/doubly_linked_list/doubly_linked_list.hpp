98a10d7f633676b2bd1414ac171f552546819338
Ruykaji <ilya.tanc@gmail.com>
2024-03-24 22:08:37 +0300

    Update doubly linked list features

--- data_structures/doubly_linked_list/doubly_linked_list.hpp
+++ data_structures/doubly_linked_list/doubly_linked_list.hpp
@@ -1,6 +1,7 @@
 #ifndef __DOUBLY_LINKED_LIST_HPP__
 #define __DOUBLY_LINKED_LIST_HPP__
 
+#include <initializer_list>
 #include <iterator>
 #include <memory>
 #include <utility>
@@ -12,8 +13,15 @@ struct Dbl_linked_list_node_base
   Dbl_linked_list_node_base* m_next;
   Dbl_linked_list_node_base* m_prev;
 
-  Dbl_linked_list_node_base(Dbl_linked_list_node_base* prev, Dbl_linked_list_node_base* next) noexcept
-      : m_prev(prev), m_next(next){};
+  Dbl_linked_list_node_base() noexcept : m_prev(nullptr), m_next(nullptr){};
+};
+
+struct Dbl_linked_list_node_header : public Dbl_linked_list_node_base
+{
+  Dbl_linked_list_node_header() : Dbl_linked_list_node_base()
+  {
+    m_next = m_prev = this;
+  };
 };
 
 template <typename Tp> struct Dbl_linked_list_node : public Dbl_linked_list_node_base
@@ -21,7 +29,8 @@ template <typename Tp> struct Dbl_linked_list_node : public Dbl_linked_list_node
   Tp m_value;
 
   template <typename... Args>
-  Dbl_linked_list_node(Args&&... args) noexcept : Dbl_linked_list_node_base(), m_value(std::forward<Args>(args)...){};
+  explicit Dbl_linked_list_node(Args&&... args) noexcept
+      : Dbl_linked_list_node_base(), m_value(std::forward<Args>(args)...){};
 };
 
 template <typename Tp> class Dbl_linked_list_iterator
@@ -50,7 +59,7 @@ public:
   }
 
   Dbl_linked_list_iterator
-  operator++() const noexcept
+  operator++() noexcept
   {
     Dbl_linked_list_iterator tmp__(*this);
     m_node = m_node->m_next;
@@ -58,14 +67,14 @@ public:
   }
 
   Dbl_linked_list_iterator&
-  operator++(int) const noexcept
+  operator++(int) noexcept
   {
     m_node = m_node->m_next;
     return *this;
   }
 
   Dbl_linked_list_iterator
-  operator--() const noexcept
+  operator--() noexcept
   {
     Dbl_linked_list_iterator tmp__(*this);
     m_node = m_node->m_prev;
@@ -73,13 +82,24 @@ public:
   }
 
   Dbl_linked_list_iterator&
-  operator--(int) const noexcept
+  operator--(int) noexcept
   {
     m_node = m_node->m_prev;
     return *this;
   }
 
-private:
+  friend bool
+  operator==(const Dbl_linked_list_iterator& lhs, const Dbl_linked_list_iterator& rhs) noexcept
+  {
+    return lhs.m_node == rhs.m_node;
+  }
+
+  friend bool
+  operator!=(const Dbl_linked_list_iterator& lhs, const Dbl_linked_list_iterator& rhs) noexcept
+  {
+    return lhs.m_node != rhs.m_node;
+  }
+
   Dbl_linked_list_node_base* m_node;
 };
 
@@ -94,24 +114,24 @@ public:
 
   Dbl_linked_list_const_iterator() : m_node(nullptr){};
 
-  Dbl_linked_list_const_iterator(Dbl_linked_list_node_base* node) noexcept : m_node(node){};
+  Dbl_linked_list_const_iterator(const Dbl_linked_list_node_base* node) noexcept : m_node(node){};
 
   Dbl_linked_list_const_iterator(const Dbl_linked_list_iterator<Tp>& itr) noexcept : m_node(itr.m_node){};
 
   reference
   operator*() const noexcept
   {
-    return static_cast<Dbl_linked_list_node<Tp>*>(m_node)->m_value;
+    return static_cast<const Dbl_linked_list_node<Tp>*>(m_node)->m_value;
   }
 
   pointer
   operator->() const noexcept
   {
-    return std::addressof(static_cast<Dbl_linked_list_node<Tp>*>(m_node)->m_value);
+    return std::addressof(static_cast<const Dbl_linked_list_node<Tp>*>(m_node)->m_value);
   }
 
   Dbl_linked_list_const_iterator
-  operator++() const noexcept
+  operator++() noexcept
   {
     Dbl_linked_list_const_iterator tmp__(*this);
     m_node = m_node->m_next;
@@ -119,14 +139,14 @@ public:
   }
 
   Dbl_linked_list_const_iterator&
-  operator++(int) const noexcept
+  operator++(int) noexcept
   {
     m_node = m_node->m_next;
     return *this;
   }
 
   Dbl_linked_list_const_iterator
-  operator--() const noexcept
+  operator--() noexcept
   {
     Dbl_linked_list_const_iterator tmp__(*this);
     m_node = m_node->m_prev;
@@ -134,32 +154,47 @@ public:
   }
 
   Dbl_linked_list_const_iterator&
-  operator--(int) const noexcept
+  operator--(int) noexcept
   {
     m_node = m_node->m_prev;
     return *this;
   }
 
-private:
-  Dbl_linked_list_node_base* m_node;
+  friend bool
+  operator==(const Dbl_linked_list_const_iterator& lhs, const Dbl_linked_list_const_iterator& rhs) noexcept
+  {
+    return lhs.m_node == rhs.m_node;
+  }
+
+  friend bool
+  operator!=(const Dbl_linked_list_const_iterator& lhs, const Dbl_linked_list_const_iterator& rhs) noexcept
+  {
+    return lhs.m_node != rhs.m_node;
+  }
+
+  const Dbl_linked_list_node_base* m_node;
 };
 
 template <typename Tp, typename Alloc = std::allocator<Tp>> class Doubly_linked_list
 {
-  static_assert(std::is_same<std::remove_cv<Tp>, Tp>::value,
+  static_assert(std::is_same<typename std::remove_cv<Tp>::type, Tp>::value,
                 "sda::Doubly_linked_list must have a non-const, non-volatile value_type");
 
   static_assert(std::is_same<typename Alloc::value_type, Tp>::value,
                 "sda::Doubly_linked_list must have the same value_type as its allocator");
 
 private:
-  template <typename Itr>
-  using Requires_input_itr = std::enable_if<std::is_convertible_v<typename Itr::iterator_tag, std::input_iterator_tag>>;
-
-  typedef Dbl_linked_list_node_base<Tp> node_base;
+  typedef Dbl_linked_list_node_base node_base;
+  typedef Dbl_linked_list_node_header node_header;
   typedef Dbl_linked_list_node<Tp> node;
   typedef typename std::allocator_traits<Alloc>::rebind_alloc<node> alloc_node;
   typedef std::allocator_traits<alloc_node> alloc_traits;
+
+  using Self = Doubly_linked_list<Tp>;
+
+  template <typename Itr>
+  using Requires_input_itr = std::enable_if<
+      std::is_convertible_v<typename std::iterator_traits<Itr>::iterator_category, std::input_iterator_tag>>;
 
 public:
   typedef Tp value_type;
@@ -171,9 +206,297 @@ public:
   typedef std::ptrdiff_t difference_type;
   typedef std::size_t size_type;
 
+  Doubly_linked_list(const allocator_type& alloc = allocator_type()) noexcept : m_head(), m_alloc(alloc_node(alloc)){};
+
+  Doubly_linked_list(size_type size, const allocator_type& alloc = allocator_type())
+      : m_head(), m_alloc(alloc_node(alloc))
+  {
+    m_default_init(size);
+  };
+
+  explicit Doubly_linked_list(size_type size, const Tp& value, const allocator_type& alloc = allocator_type())
+      : m_head(), m_alloc(alloc_node(alloc))
+  {
+    m_default_init(size, value);
+  };
+
+  Doubly_linked_list(std::initializer_list<Tp> list) : m_head(), m_alloc(alloc_node())
+  {
+    m_range_init(list.begin(), list.end());
+  };
+
+  template <typename Itr, typename = Requires_input_itr<Itr>>
+  Doubly_linked_list(Itr first, Itr last, const allocator_type& alloc = allocator_type())
+      : m_head(), m_alloc(alloc_node(alloc))
+  {
+    m_range_init(first, last);
+  };
+
+  Doubly_linked_list(const Self& list)
+      : m_head(), m_alloc(alloc_traits::select_on_container_copy_construction(list.m_alloc))
+  {
+    m_range_init(list.cbegin(), list.cend());
+  };
+
+  Doubly_linked_list(Self&& list) noexcept : m_head(), m_alloc(std::move(list.m_alloc))
+  {
+    m_move_assignment(std::move(list));
+  };
+
+  ~Doubly_linked_list()
+  {
+    clear();
+  }
+
+  Self&
+  operator=(std::initializer_list<Tp> list)
+  {
+    m_copy_assignment(list.begin(), list.end());
+    return *this;
+  }
+
+  Self&
+  operator=(const Self& list)
+  {
+    if(std::addressof(list) != this)
+      {
+        if(alloc_traits::propagate_on_container_copy_assignment::value)
+          {
+            if(alloc_traits::is_always_equal::value)
+              {
+                if(m_alloc != list.m_alloc)
+                  clear();
+              }
+
+            m_alloc = alloc_traits::select_on_container_copy_construction(list.m_alloc);
+          }
+
+        m_copy_assignment(list.cbegin(), list.cend());
+      }
+
+    return *this;
+  }
+
+  iterator
+  begin() noexcept
+  {
+    return iterator(m_head.m_next);
+  }
+
+  const_iterator
+  begin() const noexcept
+  {
+    return const_iterator(m_head.m_next);
+  }
+
+  const_iterator
+  cbegin() const noexcept
+  {
+    return const_iterator(m_head.m_next);
+  }
+
+  iterator
+  end() noexcept
+  {
+    return iterator(&m_head);
+  }
+
+  const_iterator
+  end() const noexcept
+  {
+    return const_iterator(&m_head);
+  }
+
+  const_iterator
+  cend() const noexcept
+  {
+    return const_iterator(&m_head);
+  }
+
+  template <typename Itr, typename = Requires_input_itr<Itr>>
+  iterator
+  insert(const_iterator pos, Itr first, Itr last)
+  {
+    node_base* curr__ = const_cast<node_base*>(pos.m_node);
+
+    while(first != last)
+      {
+        curr__ = m_insert(curr__, *first);
+        ++first;
+      }
+
+    return iterator(curr__);
+  }
+
+  iterator
+  erase(const_iterator first, const_iterator last)
+  {
+    node_base* curr__ = const_cast<node_base*>(first.m_node);
+    const node_base* end__ = static_cast<const node_base*>(last.m_node);
+
+    while(curr__ != end__)
+      curr__ = m_erase(curr__);
+
+    return iterator(curr__);
+  }
+
+  void
+  clear() noexcept
+  {
+    node_base* thing__ = m_head.m_next;
+
+    while(thing__->m_next != thing__)
+      thing__ = m_erase(thing__);
+  }
+
+  size_type
+  size() const noexcept
+  {
+    size_type size__ = 0;
+    const node_base* end__ = &m_head;
+    node_base* curr__ = m_head.m_next;
+
+    while(curr__ != end__)
+      {
+        curr__ = curr__->m_next;
+        ++size__;
+      }
+
+    return size__;
+  }
+
 private:
-  std::size_t m_size;
-  Dbl_linked_list_node_base m_head;
+  template <typename... Args>
+  node*
+  m_create_node(Args&&... args)
+  {
+    node* thing__ = alloc_traits::allocate(m_alloc, 1);
+
+    try
+      {
+        alloc_traits::construct(m_alloc, thing__, std::forward<Args>(args)...);
+      }
+    catch(...)
+      {
+        alloc_traits::deallocate(m_alloc, thing__, 1);
+        throw;
+      }
+
+    return thing__;
+  }
+
+  template <typename... Args>
+  void
+  m_default_init(size_type size, Args&&... args)
+  {
+    node_base* to__ = &m_head;
+
+    for(; size; --size)
+      {
+        node_base* tmp__ = to__;
+
+        to__->m_next = static_cast<node_base*>(m_create_node(std::forward<Args>(args)...));
+        to__ = to__->m_next;
+        to__->m_prev = tmp__;
+      }
+
+    node_base* head__ = &m_head;
+
+    head__->m_prev = to__;
+    to__->m_next = head__;
+  }
+
+  template <typename Itr>
+  void
+  m_range_init(Itr first, Itr last)
+  {
+    node_base* to__ = &m_head;
+
+    for(; first != last; ++first)
+      {
+        node_base* tmp__ = to__;
+
+        to__->m_next = static_cast<node_base*>(m_create_node(*first));
+        to__ = to__->m_next;
+        to__->m_prev = tmp__;
+      }
+
+    node_base* head__ = &m_head;
+
+    head__->m_prev = to__;
+    to__->m_next = head__;
+  }
+
+  template <typename Itr>
+  void
+  m_copy_assignment(Itr first, Itr last)
+  {
+    iterator cur__ = begin();
+    iterator end__ = end();
+
+    while(first != last && cur__ != end__)
+      {
+        *cur__ = *first;
+        ++cur__;
+        ++first;
+      }
+
+    if(cur__ == end__)
+      insert(end__, first, last);
+    else if(first == last)
+      erase(++cur__, end__);
+  }
+
+  void
+  m_move_assignment(Self&& list) noexcept
+  {
+    clear();
+
+    if(&list.m_head != list.m_head.m_next)
+      {
+        m_head.m_next = list.m_head.m_next;
+        m_head.m_prev = list.m_head.m_prev;
+
+        m_head.m_next->m_prev = &m_head;
+        m_head.m_prev->m_next = &m_head;
+
+        list.m_head.m_next = list.m_head.m_prev = &list.m_head;
+      }
+  }
+
+  template <typename... Args>
+  node_base*
+  m_insert(node_base* pos, Args&&... args)
+  {
+    node_base* thing__ = static_cast<node_base*>(m_create_node(std::forward<Args>(args)...));
+    node_base* prev__ = pos->m_prev;
+
+    pos->m_prev = thing__;
+    prev__->m_next = thing__;
+    thing__->m_prev = prev__;
+    thing__->m_next = pos;
+
+    return thing__;
+  }
+
+  node_base*
+  m_erase(node_base* pos) noexcept
+  {
+    node_base* next__ = pos->m_next;
+    node_base* prev__ = pos->m_prev;
+
+    alloc_traits::destroy(m_alloc, static_cast<node*>(pos));
+    alloc_traits::deallocate(m_alloc, static_cast<node*>(pos), 1);
+
+    next__->m_prev = prev__;
+    prev__->m_next = next__;
+
+    return next__;
+  }
+
+private:
+  node_header m_head;
+  alloc_node m_alloc;
 };
 
 }
--- data_structures/doubly_linked_list/test.cpp
+++ data_structures/doubly_linked_list/test.cpp
@@ -1,6 +1,127 @@
+#include <initializer_list>
+#include <string>
+#include <vector>
+
 #include <gtest/gtest.h>
 
 #include "doubly_linked_list.hpp"
+
+template <typename Tp> class Typed_doubly_linked_list_fixture : public ::testing::Test
+{
+public:
+  using List = dsa::Doubly_linked_list<Tp>;
+
+  void
+  compare_size(const List& list, List::size_type size)
+  {
+    EXPECT_EQ(list.size(), size);
+  }
+
+  template <typename Up>
+  void
+  compare_with_value(const List& list, const Up& value)
+  {
+    for(auto list_itr__ = std::cbegin(list), list_end__ = std::cend(list); list_itr__ != list_end__; ++list_itr__)
+      {
+        if(*list_itr__ != value)
+          FAIL();
+      }
+  };
+
+  template <typename Rhs_container>
+  void
+  compare_containers(const List& list, const Rhs_container& rhs)
+  {
+    EXPECT_EQ(std::distance(std::cbegin(list), std::cend(list)), std::distance(std::cbegin(rhs), std::cend(rhs)));
+
+    auto list_itr__ = std::cbegin(list);
+
+    for(auto rhs_itr__ = std::cbegin(rhs), rhs_end__ = std::cend(rhs); rhs_itr__ != rhs_end__;
+        ++list_itr__, ++rhs_itr__)
+      {
+        if(*list_itr__ != *rhs_itr__)
+          FAIL();
+      }
+  };
+
+protected:
+  static Tp s_default_value;
+  static std::initializer_list<Tp> s_list_a;
+  static std::initializer_list<Tp> s_list_b;
+};
+
+using Value_types = ::testing::Types<int, std::string>;
+TYPED_TEST_SUITE(Typed_doubly_linked_list_fixture, Value_types);
+
+// Some shortcut names
+template <typename Tp> using Init_list = std::initializer_list<Tp>;
+template <typename Tp> using Tbllf = Typed_doubly_linked_list_fixture<Tp>;
+
+// int specialization
+template <> int Tbllf<int>::s_default_value = 5;
+template <> Init_list<int> Tbllf<int>::s_list_a{ 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+template <> Init_list<int> Tbllf<int>::s_list_b{ 9, 8, 7, 6, 5, 4, 3, 2, 1, 0 };
+
+// std::string specialization
+template <> std::string Tbllf<std::string>::s_default_value = "e";
+template <> Init_list<std::string> Tbllf<std::string>::s_list_a{ "a", "b", "c", "d", "e", "f", "g", "h", "i", "j" };
+template <> Init_list<std::string> Tbllf<std::string>::s_list_b{ "j", "i", "h", "g", "f", "e", "d", "c", "b", "a" };
+
+TYPED_TEST(Typed_doubly_linked_list_fixture, Sized_default_value_constructor)
+{
+  typename TestFixture::List list__(10);
+
+  TestFixture::compare_size(list__, 10);
+  TestFixture::compare_with_value(list__, typename TestFixture::List::value_type());
+}
+
+TYPED_TEST(Typed_doubly_linked_list_fixture, Sized_provided_value_constructor)
+{
+  typename TestFixture::List list__(10, TestFixture::s_default_value);
+
+  TestFixture::compare_size(list__, 10);
+  TestFixture::compare_with_value(list__, TestFixture::s_default_value);
+}
+
+TYPED_TEST(Typed_doubly_linked_list_fixture, Initializer_list_constructor)
+{
+  typename TestFixture::List list__(TestFixture::s_list_a);
+
+  TestFixture::compare_containers(list__, TestFixture::s_list_a);
+}
+
+TYPED_TEST(Typed_doubly_linked_list_fixture, Iterator_constructor)
+{
+  std::vector<typename TestFixture::List::value_type> vec__(TestFixture::s_list_a);
+  typename TestFixture::List list__(vec__.begin(), vec__.end());
+
+  TestFixture::compare_containers(list__, vec__);
+}
+
+TYPED_TEST(Typed_doubly_linked_list_fixture, Copy_constructor)
+{
+  typename TestFixture::List list_a__(TestFixture::s_list_a);
+  typename TestFixture::List list_b__(list_a__);
+
+  TestFixture::compare_containers(list_a__, list_b__);
+}
+
+TYPED_TEST(Typed_doubly_linked_list_fixture, Move_constructor)
+{
+  typename TestFixture::List list_a__(TestFixture::s_list_a);
+  typename TestFixture::List list_b__(std::move(list_a__));
+
+  TestFixture::compare_size(list_a__, 0);
+  TestFixture::compare_containers(list_b__, TestFixture::s_list_a);
+}
+TYPED_TEST(Typed_doubly_linked_list_fixture, Initializer_list_assignment)
+{
+  typename TestFixture::List list__(TestFixture::s_list_a);
+
+  list__ = TestFixture::s_list_b;
+
+  TestFixture::compare_containers(list__, TestFixture::s_list_b);
+}
 
 int
 main(int argc, char** argv)
